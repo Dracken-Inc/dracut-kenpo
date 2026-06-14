c9abdd6a7d305eeca86621138b02efbcbb9c55a1
Gracey Blackwell <gracey@dracutkenpo.com>
2026-06-14 07:46:41 -0400

    feat: move token to .env, gitignore it

--- start-dkk.sh
+++ start-dkk.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-export SQUARE_ACCESS_TOKEN="EAAAl7HJR_JObTzGvSRFIYd4AJ3bmXHXqZoDAvHHFmAIHku07vgzVaXe1XAUjL8h"
-export DKK_LOCATION_ID="L8XCP3V5E186Y"
 cd /home/henlafon/projects/dracut-kenpo
-exec node dkk-intake-3333.js
+export $(grep -v '^#' .env | xargs)
+node dkk-intake-3333.js
