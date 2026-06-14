7e383ee4d6e29e4fed02a5e4638a6f7ed70723fa
Gracey Blackwell <gracey@dracutkenpo.com>
2026-06-14 15:55:01 -0400

    chore: remove sensitive files from repo, add to .gitignore

--- start-dkk.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/henlafon/projects/dracut-kenpo
-while IFS='=' read -r key value; do
-  [[ "$key" =~ ^#.*$ ]] && continue
-  [[ -z "$key" ]] && continue
-  export "$key"="$value"
-done < .env
-node dkk-intake-3333.js
