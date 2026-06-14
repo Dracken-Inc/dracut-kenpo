31235ab6eba9a871c301cc6789b68af43a043d12
Gracey Blackwell <gracey@dracutkenpo.com>
2026-06-14 07:43:03 -0400

    chore: remove stale start-server.sh

--- start-server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/henlafon/projects/dracut-kenpo
-export SQUARE_ACCESS_TOKEN="EAAAl-Ov2ruzpEVW678V-51rHGwQbYCDPH-ZKD4Dh8zK-Yvdg2u8GV7ssxPSLj_Y"
-export DKK_LOCATION_ID="L8XCP3V5E186Y"
-node dkk-intake-3333.js
