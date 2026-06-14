6f608992e8799eeec6998bf6a02dff20f442dab8
Gracey Blackwell <gracey@dracutkenpo.com>
2026-06-14 00:57:25 -0400

    feat: point form to Cloudflare Tunnel URL (dkk.besttherapylowell.com)

---
+++ start-dkk.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export SQUARE_ACCESS_TOKEN="EAAAl-Ov2ruzpEVW678V-51rHGwQbYCDPH-ZKD4Dh8zK-Yvdg2u8GV7ssxPSLj_Y"
+export DKK_LOCATION_ID="L8XCP3V5E186Y"
+cd /home/henlafon/projects/dracut-kenpo
+exec node dkk-intake-3333.js
